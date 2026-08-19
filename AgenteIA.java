a388d23544710756409debf827175777677a0216
leofiori06 <leofioricall@gmail.com>
2026-08-19 12:05:22 +0000

    parte 2 feito

--- AgenteIA.java
+++ AgenteIA.java
@@ -1,6 +1,9 @@
 public class AgenteIA{
     public void verificarSeguranca(String prompt) throws PromptInadequadoException{
         if (prompt != null){
+            if (prompt.length()>100){
+                throw new PromptInadequadoException("Prompt excede o limite máximo de 100 caracteres.");
+            }            
             String texto = prompt.toLowerCase();
             if (texto.contains("hackear") || texto.contains("roubar")){
                 throw new PromptInadequadoException("Prompt contém termos proibidos de segurança.");
--- Aula2Ex1.java
+++ Aula2Ex1.java
@@ -9,5 +9,17 @@ public class Aula2Ex1 {
             System.out.println("❌ Exceção capturada com sucesso!");
             System.out.println("Motivo do bloqueio: " + e.getMessage());
         }
+        System.out.println();
+
+        try{
+            System.out.println("Testando limite de 100 caracteres...");
+            String promptLongo = "Este é um texto muito longo criado exclusivamente para testar se a validação de limite de caracteres do exercício 2 está funcionndo corretamente.";
+            agente.verificarSeguranca(promptLongo);
+            System.out.println("Prompt aprovado");
+
+        } catch (PromptInadequadoException e){
+            System.out.println("❌ Exceção capturada com sucesso!");
+            System.out.println("Motivo do bloqueio: " + e.getMessage());
+        }
     }
 }
